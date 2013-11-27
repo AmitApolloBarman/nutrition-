619c193aa24994bf5dfd11efccfdb24e6d3f5c65
AmitApolloBarman <amitapollobarman@gmail.com>
2013-11-27 10:53:23 -0500

    My Nutrition App for iOS

---
+++ dating/ThirdViewController.h
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.h
+//  nutrition+
+//
+//  Created by Amit Barman on 1/4/12.
+//  Copyright (c) 2012 Apollo Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MessageUI/MFMailComposeViewController.h>
+#import <MessageUI/MFMessageComposeViewController.h>
+#import <MessageUI/MessageUI.h>
+
+@interface ThirdViewController : UIViewController <UIWebViewDelegate,UIActionSheetDelegate,MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate>
+{
+    IBOutlet UILabel *foodDesc,*foodTitle;  
+    IBOutlet UITextField *food;
+    IBOutlet UIActivityIndicatorView *activityIndicator;
+    IBOutlet UILabel *label2,*lbllink,*lblInfo;
+    IBOutlet UIWebView *wWw;
+}
+- (IBAction)loadFood;
+- (IBAction)search;
+- (IBAction)copyFood;
+- (IBAction)smsLoad;
+- (IBAction)dismissKeyboard: (id)sender;
+- (IBAction)mailLoad;
+- (IBAction)share;
+- (IBAction)clear;
+@property (nonatomic, retain) UIWebView *wWw;
+@end
